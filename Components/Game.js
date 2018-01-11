eb9165249b93ee50ad22f9d6435232c704f65689
Nadim Uddin <nadim.uddin1995@gmail.com>
2018-01-11 18:28:01 -0500

    The Game - The Game State

--- Components/Game.js
+++ Components/Game.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react'; 
 import { StyleSheet, Text, View } from 'react-native'; 
 import { StackNavigator } from 'react-navigation';
+import { emojis } from '../utils/emojis'; //new import statement up top.
 
 export default class Game extends Component { 
     constructor(props){ 
         super(props);
+        this.state = {
+            emojis: [],
+            randomQuestion: 0, 
+            score: 0
+        }
+        this.restartGame = this.restartGame.bind(this); 
+        this.pickRandomQuestion = this.pickRandomQuestion.bind(this);
+    }
 
-    }   
+    //restartGame method
+    restartGame(){
+        let randomQuestion = this.pickRandomQuestion(emojiArr);
+        let emojisArr = emojis.slice();
+        this.setState({
+            emojis: emojisArr.concat,
+            randomQuestion, 
+            score: 0
+        })
+    }
+
+    pickRandomQuestion(arr){
+        return Math.floor(Math.random() * arr.length);
+    }
+    
+    //componentDidMount lifecycle hook
+    componentDidMount(){
+        this.restartGame();
+    }
+
     render(){
         return (
-            <Text>This is my Game Component!</Text>
+            <View>
+                SCORE: {` ${this.state.score}`} 
+            </View>
         ) 
     } 
 } 
-        
-
-const styles = StyleSheet.create({ 
-    container: { 
+const styles = StyleSheet.create(
+    { container: { 
         flex: 1, 
         alignItems: 'center', 
         backgroundColor: '#AAA', 
         justifyContent: 'center' 
-    } 
-}); 
+        } 
+    });
